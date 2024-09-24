281fb62aaf54cef4e6d3b7c805c4069acfb62fec
Sebastian Castaño <scastanoa1@eafit.edu.co>
2024-09-23 20:57:27 -0500

    implementacion view recomendacion

--- DjangoProjectBase/recommendations/views.py
+++ DjangoProjectBase/recommendations/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
-from .models import Recommendation
+from movie.models import Movie
+import os
+import numpy as np
+from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
 
-# Create your views here.
+def get_embedding(text, client, model="text-embedding-3-small"):
+    text = text.replace("\n", " ")
+    return client.embeddings.create(input=[text], model=model).data[0].embedding
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
 def recommendation(request):
-    newss = Recommendation.objects.all()
-    return render(request, 'recommendations.html', {'newss':newss})
+    req = request.GET.get('req_text')
+
+    if req:
+        _ = load_dotenv('../api_keys.env') # Asegúrate de que el archivo .env esté siendo encontrado
+        api_key = os.getenv('opeanai_api_key')  # Obtén la clave de API
+        if not api_key:
+            raise ValueError("La clave de OpenAI no se encontró en las variables de entorno")
+
+        client = OpenAI(api_key=api_key)  # Usa la clave API obtenida
+
+        # Obtener todas las películas de la base de datos
+        items = Movie.objects.all()
+        
+        # Obtener el embedding de la consulta
+        emb_req = get_embedding(req, client)
+        
+        # Calcular la similitud
+        sim = []
+        for item in items:
+            emb = list(np.frombuffer(item.emb)) 
+            sim.append(cosine_similarity(emb, emb_req))
+        
+        sim = np.array(sim)
+        idx = np.argmax(sim)
+        idx = int(idx)
+        recommended_movie = items[idx]
+
+        return render(request, 'recommendations.html', {'movie': recommended_movie})
+
+    else:
+        return render(request, 'recommendations.html')
